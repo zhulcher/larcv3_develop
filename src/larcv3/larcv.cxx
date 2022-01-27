4486c58e11a2672c0ba2fccdf78c2da54f7f4a1b
Zachary Hulcher <zhulcher@gmail.com>
2022-01-26 18:24:00 -0800

    edepsim fixes

--- src/larcv3/larcv.cxx
+++ src/larcv3/larcv.cxx
@@ -4,17 +4,17 @@
 #include "larcv3/app/queueio/queueio.h"
 #include "larcv3/app/Supera/Supera.h"
 
-PYBIND11_MAKE_OPAQUE(std::vector<larcv3::SparseTensor<2> >);
-PYBIND11_MAKE_OPAQUE(std::vector<larcv3::SparseTensor<3> >);
-PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <1> >);
-PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <2> >);
-PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <3> >);
-PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <4> >);
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::SparseTensor<2> >)
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::SparseTensor<3> >)
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <1> >)
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <2> >)
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <3> >)
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <4> >)
 
-PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <1> >);
-PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <2> >);
-PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <3> >);
-PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <4> >);
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <1> >)
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <2> >)
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <3> >)
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <4> >)
 
 
 PYBIND11_MODULE(pylarcv, m) {
